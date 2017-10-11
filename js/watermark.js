08b1646ae299d9ec21fad768a26ea8e9afbe71ef
cxsagmw <18842458912@163.com>
2017-10-11 20:34:56 +0800

    Site updated: 2017-10-11 20:34:52

--- js/watermark.js
+++ js/watermark.js
@@ -28,14 +28,14 @@
             watermark_y:20,                     //水印起始位置Y轴坐标
             watermark_rows:0,                   //水印行数
             watermark_cols:0,                   //水印列数
-            watermark_x_space:100,              //水印x轴间隔
+            watermark_x_space:50,              //水印x轴间隔
             watermark_y_space:50,               //水印y轴间隔
             watermark_font:'微软雅黑',           //水印字体
             watermark_color:'black',            //水印字体颜色
-            watermark_fontsize:'12px',          //水印字体大小
-            watermark_alpha:0.15,               //水印透明度，要求设置在大于等于0.005
-            watermark_width:100,                //水印宽度
-            watermark_height:100,               //水印长度
+            watermark_fontsize:'12px',        //水印字体大小
+            watermark_alpha:0.15,            //水印透明度，要求设置在大于等于0.003
+            watermark_width:100,             //水印宽度
+            watermark_height:80,              //水印长度
             watermark_angle:15,                 //水印倾斜度数
             watermark_parent_width:0,      //水印的总体宽度（默认值：body的scrollWidth和clientWidth的较大值）
             watermark_parent_height:0,     //水印的总体高度（默认值：body的scrollHeight和clientHeight的较大值）
